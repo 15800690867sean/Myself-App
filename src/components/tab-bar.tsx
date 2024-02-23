88eccae12cbece47eb734b3675966cd8e7a3eba6
唐肖恩 <15800690867@163.com>
2024-02-24 01:19:08 +1100

    feat: ready to eject

---
+++ src/components/tab-bar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import styles from './tab-bar.module.less';
+import './tab-bar.less';
+
+export default function Tabbar(props) {
+  return (
+    <div className='tabBarContainer'>
+        Tabbar
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Tabbar from './components/tab-bar.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Tabbar />
     <div>mock tab bar</div>
     <App />
   </React.StrictMode>
