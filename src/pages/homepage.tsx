5c05a780f6d84a347717353e0c352f80b1864453
唐肖恩 <15800690867@163.com>
2024-03-09 17:11:51 +1100

    feat: set the media query

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import styles from '../styles/homepage.less';
-import bgImg from '../assets/Sydney.jpg';
+import React from "react";
+import styles from "../styles/homepage.less";
+import bgImg from "../assets/background.jpg";
 
 export default function Homepage() {
-  console.log(styles)
+  console.log(styles);
 
   return (
     <div className={styles.container}>
@@ -11,10 +11,17 @@ export default function Homepage() {
       <div className={styles.contentContainer}>
         <div className={styles.iconContainer} />
         <div className={styles.textContainer}>
-          <h1>Hello, I'm <span className={styles.target}>Sean Tang</span></h1>
+          <p>WELCOME TO MY WORLD</p>
+          <h1>
+            Hi, I'm <span className={styles.target}>Sean Tang</span>
+          </h1>
           <h1>a Frontend Developer.</h1>
+          <p>
+            I am committed to using the most professional technology to make the
+            most professional products and provide the perfect user experience.
+          </p>
         </div>
       </div>
     </div>
-  )
+  );
 }
